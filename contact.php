fb992cc29cb23d7a294cfe5426df58ecf6f56e29
Matias Rico <71736647+MatiasRico@users.noreply.github.com>
2020-11-15 21:42:08 -0300

    Add files via upload

--- contact.php
+++ contact.php
@@ -9,10 +9,10 @@ function isEmail($email) {
 
 if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 
-$first_name     = $_POST['first_name'];
-$last_name     = $_POST['last_name'];
+$first_name     = $_POST['Nombre'];
+$last_name     = $_POST['Apellido'];
 $email    = $_POST['email'];
-$phone   = $_POST['phone'];
+$phone   = $_POST['Telefono'];
 $select_price   = $_POST['select_price'];
 $select_service   = $_POST['select_service'];
 $subject  = $_POST['subject'];
